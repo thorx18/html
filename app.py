f35582109df072cb1041c6b3f31a07b6ef49e27f
thorx18 <shubhammali26012004@gmail.com>
2026-02-14 15:23:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def add(a, b):
 
 # Simple test
 if add(2, 3) == 5:
-    print("Test Passed Bro , Well Done ✅")
+    print("Test Passed Bro , Well Done man✅")
 else:
     print("Test Failed Bro , Next time ❌")
     exit(1)
