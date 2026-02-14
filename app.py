2329d45382ef3898efa199e4f76d849886d40936
thorx18 <shubhammali26012004@gmail.com>
2026-02-14 16:31:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-#def add(a, b):
+def add(a, b):
     return a + b
 
 
 # Simple test
-#if add(2, 3) == 5:
+=if add(2, 3) == 5:
     print("Test Passed Bro , Well Done man✅")
 else:
     print("Test Failed Bro , Next time ❌")
